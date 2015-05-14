c7cafbd1b882030ca76c33af861e6d9b05cb5b9a
Andrew Neill <ipaqbxs@yahoo.com>
2015-05-14 17:54:55 -0600

    latest update

--- src/spacenav_control.cpp
+++ src/spacenav_control.cpp
@@ -1,14 +1,18 @@
-// Teleoperation of ABB robot by a 3d mouse
-// The idea is, use a 3d mouse to jog a robot, giving 6 axis control
+/*  Teleoperation of ABB robot by a 3d mouse
+	The idea is, use a 3d mouse to jog a robot, giving 6 axis control
+
+	TODO: 
+	1. Add ability to do joint jog
+	2. Additional axes (positioner etc)
+	3. 
+*/
 
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
-// ???#include <sensor_msgs/trajectory_msgs.h>
-// ros industrial messages?
+#include <moveit/move_group_interface/move_group.h>
+#include "geometry_msgs/Point.h"
 
-// how do I jog the robot? MoveIt maybe?
-
-class JogRobot
+class JogRobotClass
 {
 public:
 	JogRobot();
@@ -17,6 +21,7 @@ private:
 	void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
 	ros::NodeHandle nh_;
+	moveit::planning_interface::MoveGroup arm;
 
 	int linear_, angular_;
 	double l_scale_, a_scale_;
@@ -25,30 +30,60 @@ private:
 
 };
 
+// bool JogRobotClass::moveToPose(geometry_msgs::Pose goalPose, std::string link){
+// 	bool isPlanningSuccess;
+// 	bool isMoveSuccess = false;
+// 	moveit::planning_interface::MoveGroup::Plan my_plan;
+// 	arm.setStartState(*arm.getCurrentState());
+// 	arm.setPoseTarget(goalPose, link);
 
-JogRobot::JogRobot():
+// 	// call the planner to compute the plan and visualize it
+// 	isPlanningSuccess = arm.plan(my_plan);
+
+// 	/* *****************  FOR DEBUGGING ***************/
+// 	ROS_INFO("Visualizing plan %s", isPlanningSuccess?"":"FAILED");
+
+// 	if(isPlanningSuccess){
+// 		isMoveSuccess = arm.execute(my_plan);
+// 		ros::Duration(0.5).sleep();
+// 	}
+// 	return isPlanningSuccess && isMoveSuccess;
+// }
+
+JogRobotClass::JogRobot():
 linear_(1),
-angular_(2)
+angular_(2),
+moveit_config_(3)
 {
 	nh_.param("axis_linear", linear_, linear_);
 	nh_.param("axis_angular", angular_, angular_);
 	nh_.param("scale_angular", a_scale_, a_scale_);
 	nh_.param("scale_linear", l_scale_, l_scale_);
+	nh_.param("movit_config", moviet_config_);
 
 	vel_pub_ = nh_.advertise<industrial::Velocity>("trajectory_msgs/JointTrajectoryPoint", 1); 
-
 	joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &JogRobot::joyCallback, this);
 
 }
 
-void JogRobot::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+void JogRobotClass::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
+	bool planSuccess;
 	
-	industrial::Velocity vel;
+	//move_group_interface::MoveGroup group("irb1600id_moveit_config");
+	move_group_interface::MoveGroup group(moveit_config_);
+	arm("manipulator");
 	
-	vel.angular = a_scale_*joy->axes[angular_];
-	vel.linear = l_scale_*joy->axes[linear_];
-	vel_pub_.publish(vel);
+	//industrial::Velocity vel;
+	geometry_msgs::Pose pose = arm.getCurrentPose().cartesianPosition;
+	cartesianPosition.position.x += a_scale_*joy->axes[angular_];
+	cartesianPosition.position.x += l_scale_*joy->axex[linear_];
+
+	group.setPoseTarget(cartesianPosition);
+	planSuccess = arm.plan(thePlan);
+	if(isPlanningSuccess)
+		arm.execute(thePlan);
+	
 }
 
 int main(int argc, char** argv)
